fc3df0fe5e20daf805acceb7e6caf00bd759c652
gibeulah <gibeulah@microsoft.com>
2018-11-29 17:13:14 +0530

    First Commit

--- InplaceDuplicatesRemovalUsingHashing/Program.cs
+++ InplaceDuplicatesRemovalUsingHashing/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace InplaceDuplicatesRemovalUsingHashing
 {
@@ -6,7 +7,57 @@ namespace InplaceDuplicatesRemovalUsingHashing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Inplace duplicates' removal using Hashing!");
+            Console.WriteLine("------------------------------------------");
+
+            Console.WriteLine("Enter the input string");
+            String inputString = Console.ReadLine();
+            try
+            {
+                String outputString = GetInplaceDuplicateRemoval(inputString);
+                if (!string.IsNullOrEmpty(outputString))
+                {
+                    Console.WriteLine("---The resultant string is " + outputString);
+                }
+                else
+                {
+                    Console.WriteLine("The input string is empty! Nothing to process!");
+                }
+            }
+            catch (Exception exception) {
+                Console.WriteLine("Thrown exception is "+exception.Message);
+            }
+
+
+            Console.ReadLine();
+        }
+
+        private static string GetInplaceDuplicateRemoval(String inputString) {
+            if (string.IsNullOrEmpty(inputString)) {
+                return inputString;
+            }
+            if (string.IsNullOrEmpty(inputString.Trim())) {
+                return inputString.Trim();
+            }
+
+            int newIndex = 0;
+            bool[] hashMap = new bool[256];
+            for (int i = 0; i < inputString.Length; i++) {
+                if (hashMap[inputString[i]] == false) {
+                    hashMap[inputString[i]] = true;
+                    StringBuilder stringBuilder = new StringBuilder(inputString);
+                    stringBuilder[newIndex] = inputString[i];
+                    inputString = stringBuilder.ToString();
+                    newIndex++;
+                }
+            }
+            while (newIndex < inputString.Length) {
+                StringBuilder stringBuilder = new StringBuilder(inputString);
+                stringBuilder[newIndex] = '\0';
+                inputString = stringBuilder.ToString();
+                newIndex += 1;
+            }
+            return inputString;
         }
     }
 }
